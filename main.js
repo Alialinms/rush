35f2f8b61ba679cbc478774f42c4fe7b83f101d8
Ali Alinms <macowner@Macs-MacBook-Pro.local>
2025-09-08 01:35:58 +0300

    The Game Is Done

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
+let parent_listItems = document.querySelector(".game .container ul");
 let listItems = document.querySelectorAll(".game .container ul li");
+let list_level = document.querySelectorAll(".ready .container ul li");
+let timerDisplay = document.querySelector(".game .container span");
 let flippedCards = [];
-let pre = 0;
-let i = 0;
 let clic = 0;
+let n_level;
+let sultan = 0;
+let timeLeft;
+let countdown;
+
 
 function toggleBeforeEffect() {
+    scrollDown();
+    startTimer();
+    document.querySelector(".ready").remove();
     listItems.forEach(li => 
         {
             li.classList.add("hide-before");
@@ -14,11 +23,12 @@ function toggleBeforeEffect() {
                 li.classList.remove("hide-before");
             }, 2000);
         });
-    document.querySelector(".question .container p").remove();
+    document.querySelector(".question").remove();
     setTimeout(() =>
     {
+        sultan = 1;
         clic = 1
-    },1000);
+    },2000);
 }
 
 function shuffle(array)
@@ -37,8 +47,8 @@ function shuffle(array)
 
 function makeCardGrid(column, row)
 {
+    document.querySelector(".game .container span").style.display="block";
     let card_grid = document.querySelector(".game .container ul");
-    
     let array = [];
     const max = column * row;
     const min = 0;
@@ -58,59 +68,164 @@ function makeCardGrid(column, row)
     images.forEach(img => {
         img.style.width = (image_width) + "px";
     });
-    listItems = document.querySelectorAll(".game .container ul li")
+    listItems = document.querySelectorAll(".game .container ul li");
+    play_game(max / 2);
 }
 
-makeCardGrid(4, 4)
-
-listItems.forEach(function(e) {
-    e.addEventListener("click", function() {
-        if (i < 2 && clic == 1)
-        {
-            i++;
-            console.log(i);
-            e.classList.add("hide-before");
-            const imgSrc = e.querySelector("img").getAttribute("src");
-            flippedCards.push(imgSrc);
-            if (flippedCards.length === 2)
-            {
-                if (flippedCards[0] === flippedCards[1])
+function play_game(len)
+{
+    let pre = 0;
+    let i = 0;
+    let id;
+    let check = [];
+    let count_win = 0;
+    listItems.forEach(function(e) {
+        e.addEventListener("click", function() {
+            for (const ele of check) {
+                if (ele == e)
                 {
-                    e.classList.add("action-win");
-                    if (pre != 0)
-                    {
-                        pre.classList.add("action-win");
-                    }
-                    setTimeout(() => {
-                        e.classList.remove("action-win");
-                        pre.classList.remove("action-win");
-                        flippedCards = [];
-                    }, 1000);
-                } 
-                else
-                {
-                    e.classList.add("action-loser");
-                    if (pre != 0)
-                    {
-                        pre.classList.add("action-loser");
-                    }
-                    setTimeout(() => {
-                        e.classList.remove("action-loser");
-                        pre.classList.remove("action-loser");
-                        pre.classList.remove("hide-before");
-                        e.classList.remove("hide-before");
-                        flippedCards = [];
-                    }, 1000);
+                    clic = 0;
                 }
-                setTimeout(() => {
-                    i = 0;
-                },1000);
             }
-            else
+            if (sultan == 1)
             {
-                pre = e;
+                if (i < 2 && clic == 1 && id != e)
+                    {
+                        id = e;
+                        i++;
+                        e.classList.add("hide-before");
+                        const imgSrc = e.querySelector("img").getAttribute("src");
+                        flippedCards.push(imgSrc);
+                        if (flippedCards.length === 2)
+                        {
+                            id = 0;
+                            if (flippedCards[0] === flippedCards[1])
+                            {
+                                e.classList.add("action-win");
+                                if (pre != 0)
+                                {
+                                    pre.classList.add("action-win");
+                                }
+                                count_win++;
+                                console.log(count_win);
+                                if (count_win == len)
+                                {
+                                    clearInterval(countdown);
+                                    showWinPopup();
+                                }
+                                check.push(e);
+                                check.push(pre);
+                                setTimeout(() => {
+                                    e.classList.remove("action-win");
+                                    pre.classList.remove("action-win");
+                                    flippedCards = [];
+                                }, 1000);
+                            } 
+                            else
+                            {
+                                e.classList.add("action-loser");
+                                if (pre != 0)
+                                {
+                                    pre.classList.add("action-loser");
+                                }
+                                setTimeout(() => {
+                                    e.classList.remove("action-loser");
+                                    pre.classList.remove("action-loser");
+                                    pre.classList.remove("hide-before");
+                                    e.classList.remove("hide-before");
+                                    flippedCards = [];
+                                }, 1000);
+                            }
+                            setTimeout(() => {
+                                i = 0;
+                            },1000);
+                        }
+                        else
+                        {
+                            pre = e;
+                        }
+                    }
+                    clic = 1;
             }
-        }
+        });
     });
-});
+}
 
+list_level.forEach(li =>
+    {
+        
+        let level = li.textContent;
+        li.addEventListener("click", function()
+        {
+            parent_listItems.innerHTML="";
+            if (n_level)
+            {
+                n_level.classList.remove("choose");
+            }
+            document.querySelector(".question").style.display="none";
+            if (level == "Easy")
+            {
+                n_level = li;
+                parent_listItems.style.width="400px";
+                timerDisplay.innerHTML="10";
+                timeLeft = 10;
+                makeCardGrid(2, 2);
+                li.classList.add("choose");
+                document.querySelector(".question").style.display="block";
+                
+            }
+            if (level == "Medium")
+            {
+                n_level = li;
+                parent_listItems.style.width="800px";
+                timerDisplay.innerHTML="15";
+                timeLeft = 15;
+                makeCardGrid(4, 2);
+                li.classList.add("choose");
+                document.querySelector(".question").style.display="block";
+            }
+            if (level == "Difficult")
+            {
+                n_level = li;
+                parent_listItems.style.width="700px";
+                timerDisplay.innerHTML="30";
+                timeLeft = 30;
+                makeCardGrid(4, 4);
+                li.classList.add("choose");
+                document.querySelector(".question").style.display="block";
+            }
+        })
+    }
+    )
+
+
+function showWinPopup()
+{
+    document.getElementById("win-popup").style.display = "block";
+}
+    
+function restartGame()
+{
+    location.reload();
+}
+    
+function scrollDown()
+{
+    window.scrollBy({
+        top: 500,
+        behavior: "smooth"
+});
+}
+
+function startTimer() {
+    countdown = setInterval(() => {
+        timeLeft--;
+        timerDisplay.textContent = timeLeft;
+        
+        if (timeLeft <= 0) {
+            clearInterval(countdown);
+            timerDisplay.style.display="none";
+            document.getElementById("loss-popup").style.display = "block";
+        }
+    }, 1000);
+}
